f9d369f37142221f0149f4ed2af74ca267174414
wats0nUnMEduB13 <nghtrsnl@gmail.com>
2024-10-09 12:34:09 -0400

    added more clear comments and doubel checked styles, scripts and links all worked correctly

--- Play2Learn Website/src/assets/register-script.js
+++ Play2Learn Website/src/assets/register-script.js
@@ -1,13 +1,13 @@
-//Focus on FIRST principles for efficient development, comment MORE for readibility + maintainability
+// focus on FIRST principles for efficient development, comment more for readibility + maintainability
 
 
-// Login & Register
-const needAccount = document.getElementById("btn-reg");
-const registerForm = document.getElementById("register");
-const loginForm = document.getElementById("login-form"); 
-
+// login & register logic
+const needAccount = document.getElementById("btn-reg"); // get the "Need an Account?" link
+const registerForm = document.getElementById("register"); // get the registration form
+const loginForm = document.getElementById("login-form"); // get the login form
+// add a click event listener to the "Need an Account?" link
 needAccount.addEventListener("click", function() {
-  loginForm.style.display = "none";
-  registerForm.style.display = "block";
+  loginForm.style.display = "none"; // hide the login form
+  registerForm.style.display = "block"; // show the registration form
 });
 
